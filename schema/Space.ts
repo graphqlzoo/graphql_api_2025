de874d7e593a4b366e59afb86795cd84dbb24b27
DUPLESSIS Alexis <98809630+meizarkos@users.noreply.github.com>
2025-09-09 12:48:32 +0200

    Space

--- schema/Space.ts
+++ schema/Space.ts
@@ -1,23 +1,36 @@
-import { GraphQLEnumType, GraphQLNonNull, GraphQLObjectType } from "graphql";
-import { GraphQLID, GraphQLString } from "graphql/type/scalars";
+import { GraphQLEnumType, GraphQLList, GraphQLNonNull, GraphQLObjectType } from "graphql";
+import { GraphQLBoolean, GraphQLID, GraphQLString } from "graphql/type/scalars";
 
-const climateEnum = new GraphQLEnumType({
-  name: "Climate",
+const spaceTypeEnum = new GraphQLEnumType({
+  name: "SpaceTypeEnum",
   values: {
     HOT: { value: "Hot" },
+    SAVANA: { value: "Savana" },
+    TAIGA: { value: "Taiga" },
+    TUNDRA: { value: "Tundra" },
+    MOUNTAIN: { value: "Mountain" },
+    DESERT: { value: "Desert" },
     COLD: { value: "Cold" },
+    HUMID: { value: "Humid" },
+    TROPICAL : { value: "Tropical" },
     OTHER: { value: "Other" },
-    UNKNOWN: { value: "Unknown" }
   }
 });
 
 export const spaceType = new GraphQLObjectType({
-  name: 'Planet',
+  name: 'Space',
   fields: {
     id: {
       type: GraphQLID,
     },
     name: { type: new GraphQLNonNull(GraphQLString) },
     description: { type: new GraphQLNonNull(GraphQLString) },
+    images : { type : new GraphQLList(GraphQLString) },
+    types : { type : new GraphQLList(spaceTypeEnum) },
+    openHours : { type: GraphQLString },
+    closeHours : { type: GraphQLString },
+    disabled: { type: GraphQLNonNull(GraphQLBoolean) },
+    createdAt : { type: GraphQLString },
+    updatedAt : { type: GraphQLString },
   }
 });
