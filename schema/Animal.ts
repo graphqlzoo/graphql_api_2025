b7e7c9731d2b5163c2eaa9cdc2743aa12a44af94
rishi <balasubramaniam.rishi@gmail.com>
2025-09-21 15:53:55 +0200

    Fix Animal

--- schema/Animal.ts
+++ schema/Animal.ts
@@ -1,5 +1,38 @@
-import { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLBoolean, GraphQLEnumType, GraphQLList } from "graphql";
+import { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLBoolean, GraphQLEnumType, GraphQLList, GraphQLNonNull } from "graphql";
 import { speciesType } from "./Species";
 
-
+export const animalType = new GraphQLObjectType({
+  name: 'Animal',
+  fields: {
+    id: {
+      type: new GraphQLNonNull(GraphQLString),},
+    space: {
+      type: new GraphQLNonNull(speciesType),
+    },
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  
+    description: {
+      type: GraphQLString,
+    },
+    image : {
+      type: new GraphQLList(GraphQLString),
+    },
+    
+    species : {
+      type: speciesType,
+    },
+    bornOn: {
+      type: GraphQLString,
+    },
+    createdAt: 
+    {
+      type: GraphQLString,
+    },
+    updatedAt: {
+      type: GraphQLString,
+    },
+  }
+});
 
